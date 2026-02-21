3e91617754e77a9a36af507c9f9eb987370a2552
Maria Júlia Vieira <mariajuliavieira@MacBook-Air-de-Maria.local>
2026-02-20 22:44:34 -0300

    fix: user name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://seu-usuario.github.io',
+  site: 'https://JuliaOli.github.io',
   base: '/juliaOli-portfolio/',
   output: 'static',
   vite: {
